bff55a3ead6647ce61b17712a20f66151a32d99b
Fangzhou Sun <fzsun316@gmail.com>
2014-10-13 21:42:43 -0500

    Added a dialog to add a new movie

--- Client/app/src/main/java/org/magnum/videoup/client/VideoListActivity.java
+++ Client/app/src/main/java/org/magnum/videoup/client/VideoListActivity.java
@@ -9,9 +9,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.app.Dialog;
 
 import org.magnum.mobilecloud.video.client.VideoSvcApi;
 import org.magnum.mobilecloud.video.repository.Video;
@@ -21,6 +26,14 @@ import butterknife.InjectView;
 import butterknife.OnClick;
 
 public class VideoListActivity extends Activity {
+
+    Dialog custom;
+    Button savebtn;
+    Button canbtn;
+    EditText editText_name;
+    EditText editText_url;
+    EditText editText_duration;
+
 
 	@InjectView(R.id.videoList)
 	protected ListView videoList_;
@@ -79,9 +92,78 @@ public class VideoListActivity extends Activity {
 
     @OnClick(R.id.button1)
     public void addNewMovie() {
-        startActivity(new Intent(
-                VideoListActivity.this,
-                AddMovieActivity.class));
+        custom = new Dialog(VideoListActivity.this);
+        custom.setContentView(R.layout.dialog_add_movie);
+
+        custom.setTitle("Add New Movie");
+        savebtn = (Button)custom.findViewById(R.id.savebtn);
+        canbtn = (Button)custom.findViewById(R.id.canbtn);
+
+        editText_name = (EditText)custom.findViewById(R.id.mName);
+        editText_url = (EditText)custom.findViewById(R.id.mUrl);
+        editText_duration = (EditText)custom.findViewById(R.id.mDuration);
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+
+            String movieName,movieUrl, movieDuration;
+
+            @Override
+            public void onClick(View view) {
+                // TODO Auto-generated method stub
+                movieName = editText_name.getText().toString();
+                movieUrl = editText_url.getText().toString();
+                movieDuration = editText_duration.getText().toString();
+
+                final Video v = new Video(movieName, movieUrl, Long.parseLong(movieDuration));
+                final VideoSvcApi svc = VideoSvc.getOrShowLogin(VideoListActivity.this);
+                if (svc != null) {
+                    CallableTask.invoke(new Callable<Collection<Video>>() {
+
+                        @Override
+                        public Collection<Video> call() throws Exception {
+                            svc.addVideo(v);
+
+                            return svc.getVideoList();
+                        }
+                    }, new TaskCallback<Collection<Video>>() {
+                        @Override
+                        public void success(Collection<Video> result) {
+                            List<String> names = new ArrayList<String>();
+                            for (Video v : result) {
+                                names.add(v.getName());
+                            }
+                            videoList_.setAdapter(new ArrayAdapter<String>(
+                                    VideoListActivity.this,
+                                    android.R.layout.simple_list_item_1, names));
+                        }
+
+                        @Override
+                        public void error(Exception e) {
+                            Toast.makeText(
+                                    VideoListActivity.this,
+                                    "Unable to fetch the video list, please login again.",
+                                    Toast.LENGTH_SHORT).show();
+
+                            startActivity(new Intent(VideoListActivity.this,
+                                    LoginScreenActivity.class));
+                        }
+                    });
+                }
+
+                custom.dismiss();
+            }
+
+        });
+        canbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // TODO Auto-generated method stub
+                custom.dismiss();
+
+            }
+        });
+        custom.show();
     }
 
 }
